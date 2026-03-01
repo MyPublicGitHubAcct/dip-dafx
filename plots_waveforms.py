a51d53543dd5d49c262c2f34c74e35f52e3998cc
Mark Wagner <you@example.com>
2026-03-01 11:38:36 -0500

    Update plot, closer to right...

--- plots_waveforms.py
+++ plots_waveforms.py
@@ -1,16 +1,19 @@
 import numpy as np
 
-from util_waveforms import wv_sine
-# from matplotlib import pyplot as plt
-from util_plotting import plot_single_waveform
+from util_waveforms import wv_sine, wv_sawtooth
+from util_plotting import plot_single_waveform, plot_fft_mag_phs
 
 def plot_sine():
-    fs = 100
+    fs = 48000
+    nyquist_frequency = fs / 2
     seconds = 1
     t = np.linspace(0, seconds, fs * seconds)
-    freq = 1
-    signal = wv_sine(t, freq)
-    plot_single_waveform("A Sine Wave", "Time", "Amplitude", t, signal)
+    freq = 440
+    # signal = wv_sine(t, freq)
+    signal = wv_sawtooth(t, freq)
+
+    # plot_single_waveform(t, signal, "A Sine Wave", "Time", "Amplitude")
+    plot_fft_mag_phs(signal, width=nyquist_frequency, fs=fs, fft_size=fs)
 
 if __name__ == '__main__':
     plot_sine()
--- util_plotting.py
+++ util_plotting.py
@@ -3,44 +3,45 @@ from scipy.fft import fft, ifft, fftfreq, fftshift
 import matplotlib.pyplot as plt
 
 
-def plot_single_waveform(title, x_label, y_label, t, X):
-    '''
+def plot_single_waveform( time, signal, title, x_label='Time', y_label='Magnitude'):
+    """
     Call like
-        plot_waveform('My Title', 'my x label', 'my y label', t, X)
-    '''
+    plot_waveform('My Title', 'my x label', 'my y label', t, X)
+    """
     fig, ax1 = plt.subplots(1)
     ax1.set_title(title)
     ax1.set_xlabel(x_label)
     ax1.set_ylabel(y_label)
-    ax1.plot(t, X)
+    ax1.plot(time, signal)
     plt.tight_layout()
     plt.show()
 
 
-def plot_fft_mag_phs(signal, \
-                     mtitle='Magnitude response', mx_label='frequencies (f)',
-                     my_label='Magnitude', \
-                     ptitle='Phase response', px_label='frequencies (f)',
-                     py_label='Phase', \
-                     width=100, fs=41000, fft_size=41000):
-    '''
+def plot_fft_mag_phs(signal,
+                     axis_one_title='Magnitude response',
+                     mx_label='frequencies (f)',
+                     my_label='Magnitude',
+                     axis_two_title='Phase response',
+                     px_label='frequencies (f)',
+                     py_label='Phase',
+                     width=41000/2, fs=41000, fft_size=41000):
+    """
     Call like
-        plot_fft_mag_phs('freq domain', 'frequencies (f)', 'Magnitude', 'phase response', \
-                         'frequencies (f)', 'phase', fs, fs, X)
-    '''
+    plot_fft_mag_phs(signal, width=fs/2, fs=fs, fft_size=fs)
+    """
     fig, (ax1, ax2) = plt.subplots(2)
     df = fs / fft_size  # frequency resolution
-    X_fft = 1 / fft_size * fftshift(fft(signal, fft_size))  # fft of the signal
-    sampleIndex = np.arange(start=-fft_size // 2, stop=fft_size // 2)  # raw index for
+    x_fft = 1 / fft_size * fftshift(fft(signal, fft_size))  # fft of the signal
+    sample_index = np.arange(-fft_size // 2, fft_size // 2)  # raw index for
     # FFT plot
-    xf = sampleIndex * df  # x-axis converted to freq
-    phase = np.arctan2(np.imag(X_fft), np.real(X_fft)) * 180 / np.pi
-    ax1.set_title(mtitle)
+    xf = sample_index * df  # x-axis converted to freq
+    phase = np.arctan2(np.imag(x_fft), np.real(x_fft)) * 180 / np.pi
+    ax1.set_title(axis_one_title)
     ax1.set_xlabel(mx_label)
     ax1.set_ylabel(my_label)
-    ax1.stem(xf, abs(X_fft))
-    ax1.set_xlim(-width, width)
-    ax2.set_title(ptitle)
+    ax1.stem(xf, abs(x_fft))
+    ax1.set_xlim(0, width)
+    ax2.set_title(axis_two_title)
     ax2.set_xlabel(px_label)
     ax2.set_ylabel(py_label)
     ax2.plot(xf, phase)
